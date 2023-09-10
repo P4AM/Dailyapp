c0297e172cb5881b217e9bb8895ce40f966e0f69
Patrick Mallqui <agustin.pmall@gmail.com>
2023-09-09 19:42:02 -0500

    Add project files.

---
+++ Dailyapp/Data/ConexionMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Dailyapp.Data
+{
+    internal class ConexionMain
+    {
+        public static String conexion = "Data Source = 192.168.0.101; Initial Catalog = ;Integrate Security = False; User Id = dailyadmin; Password = 12345678";
+        public static SqlConnection conectar = new SqlConnection(conexion);
+        public static void Abrir()
+        {
+            if (conectar.State == ConnectionState.Closed)
+            {
+                conectar.Open();
+            }
+        }
+
+        public static void Cerrar()
+        {
+            if (conectar.State == ConnectionState.Open)
+            {
+                conectar.Close();
+            }
+        }
+    }
+}
